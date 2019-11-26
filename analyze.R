b7cb40e4b153a45a254ef031909bd66e70eddda5
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-11-26 11:06:01 -0600

    Updated analysis.
    Cache downloaded census data.

--- analyze.R
+++ analyze.R
@@ -1,5 +1,5 @@
 library(pacman)
-p_load(tidyverse, magrittr, lubridate, readxl, censusapi, janitor)
+p_load(tidyverse, magrittr, lubridate, readxl, broom, janitor, censusapi)
 p_load_gh("walkerke/tidycensus")
 
 data_dir = here::here("data")
@@ -26,10 +26,17 @@ use_data <- use_data %>%
   left_join(select(emission_factors, state, e_factor), by = "state") %>%
   mutate(emissions = residential * e_factor)
 
-state_pop <- getCensus("pep/int_charagegroups", 2000,
-                               vars=c("POP", "DATE_", "DATE_DESC", "GEONAME"),
-                               region="state:*") %>%
-  as_tibble() %>% clean_names()
+state_pop_file <- file.path(data_dir, "state_pop_1.Rds")
+
+if (file.exists(state_pop_file)) {
+  state_pop <- read_rds(state_pop_file)
+} else {
+  state_pop <- getCensus("pep/int_charagegroups", 2000,
+                         vars=c("POP", "DATE_", "DATE_DESC", "GEONAME"),
+                         region="state:*") %>%
+    as_tibble() %>% clean_names()
+  write_rds(state_pop, state_pop_file)
+}
 
 sp1 <- state_pop %>%
   mutate(date = date_desc %>% str_replace_all("([0-9]) *[A-Za-z].*$", "\\1") %>% mdy) %>%
@@ -37,20 +44,35 @@ sp1 <- state_pop %>%
   mutate(year = year(date), pop = as.numeric(pop)) %>%
   select(year, state_code = state, pop)
 
-state_pop_2 <- map_df(fips, ~getCensus("pep/int_charagegroups", 1990,
-                         vars=c("POP", "YEAR", "STATE", "COUNTY", "AGEGRP",
-                                "RACE_SEX", "HISP"),
-                         region="county:*", regionin=str_c("state:", .x))) %>%
-  as_tibble() %>% clean_names() %>% mutate(pop = as.numeric(pop))
+state_pop_2_file <- file.path(data_dir, "state_pop_2.Rds")
 
-sp2 <- state_pop_2 %>% group_by(year, state) %>%
-  summarize(pop = sum(pop, na.rm = TRUE)) %>% ungroup() %>%
-  mutate(year = 1900 + as.integer(year)) %>%
-  filter(year >= 1990) %>%
-  rename(state_code = state)
+if (file.exists(state_pop_2_file)) {
+  state_pop_2 <- read_rds(state_pop_2_file)
+} else {
+  county_pop_2 <- map_df(fips, ~getCensus("pep/int_charagegroups", 1990,
+                                         vars=c("POP", "YEAR", "STATE", "COUNTY", "AGEGRP",
+                                                "RACE_SEX", "HISP"),
+                                         region="county:*", regionin=str_c("state:", .x))) %>%
+    as_tibble() %>% clean_names() %>% mutate(pop = as.numeric(pop))
+  state_pop_2 <- county_pop_2 %>% group_by(year, state) %>%
+    summarize(pop = sum(pop)) %>% ungroup() %>%
+    mutate(year = 1900 + as.integer(year)) %>%
+    filter(year >= 1990) %>% rename(state_code = state)
 
-state_pop_3 <- get_estimates("state", "population", time_series = TRUE) %>%
-  clean_names() %>% rename(state = geoid)
+  write_rds(state_pop_2, state_pop_2_file)
+}
+
+sp2 <- state_pop_2
+
+state_pop_3_file <- file.path(data_dir, "state_pop_3.Rds")
+
+if (file.exists(state_pop_3_file)) {
+  state_pop_3 <- read_rds(state_pop_3_file)
+} else {
+  state_pop_3 <- get_estimates("state", "population", time_series = TRUE) %>%
+    clean_names() %>% rename(state = geoid)
+  write_rds(state_pop_3, state_pop_3_file)
+}
 
 sp3 <- state_pop_3 %>% filter(date > 3) %>%
   mutate(year = 2010 + date - 3, date = make_date(year, 7, 1)) %>%
@@ -60,7 +82,7 @@ sp3 <- state_pop_3 %>% filter(date > 3) %>%
 sp <- bind_rows(sp1, sp2, sp3)
 
 sp_us <- sp %>% group_by(year) %>%
-  summarize(state_code = "00", pop = sum(pop, na.rm = TRUE))
+  summarize(state_code = "00", pop = sum(pop))
 
 sp <- bind_rows(sp, sp_us)
 
@@ -88,21 +110,31 @@ fits <- us_total %>%
                       labels = c("Linear extrapolation from pre-2007 data",
                                  "Quadratic fit to all data")))
 
+line_levels <- levels(fits$Fit)
+
+line_values <- set_names(c("dashed", "solid"), line_levels)
+
 ggplot(us_total, aes(x = year, y = per_capita_use)) +
   geom_point() +
-  geom_line(aes(color = Fit), data = fits, size = 1) +
+  geom_line(aes(color = Fit, linetype = Fit), data = fits, size = 1) +
   scale_color_brewer(palette = "Dark2", name = NULL) +
-  # scale_y_continuous(limits=c(NA, 2.4), breaks = seq(0, 5, 0.2)) +
-  # scale_x_continuous(limits = c(1990,2020)) +
+  scale_linetype_manual(values = line_values, name = NULL) +
   labs(x = "Year",
        y = "MWh per year",
        title = "Per Capita Electricity Consumption") +
-  theme_bw(base_size = 15) +
-  theme(legend.position = c(0.1, 0.95),
-        legend.justification = c(0,1),
-        legend.margin = margin(0, 0, 5, 0),
+  theme_bw(base_size = 16) +
+  theme(legend.position = c(0.07, 0.9),
+        legend.justification = c(0,0.5),
+        legend.key.height = unit(0.5, "lines"),
+        legend.key.width = unit(2.5, "lines"),
+        legend.margin = margin(0, 0, 0, 0),
         legend.box.margin = margin(0, 0, 0, 0),
         legend.title = element_blank())
+
+ggsave(filename = "electricity_figure.png", dpi = 600, width = 6, height = 6,
+       units = "in")
+ggsave(filename = "electricity_figure.pdf", dpi = 600, width = 6, height = 6,
+       units = "in")
 
 emissions_change <- df %>% filter(state != "US") %>%
   select(year, state, sector, per_capita_use, e_factor, pop) %>%
